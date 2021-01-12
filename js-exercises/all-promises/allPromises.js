a4201dc9fe587bfdcb94c3e8d0047177e35f1c94
Deepanshu Gupta <guptadeepanshu21@gmail.com>
2021-01-12 22:39:57 +0530

    solved all-promises

--- js-exercises/all-promises/allPromises.js
+++ js-exercises/all-promises/allPromises.js
@@ -1,10 +1,16 @@
-// const allPromises = (args) => new Promise(resolve => {
-//   if (args && args.length) {
-//     resolve(Promise.all(args));
-//   }
-// });
-
-// using default param value
-const allPromises = (args = []) => Promise.all(args);
+const allPromises = (args) => new Promise((resolve, reject) => {
+  if (args && args.length) {
+    const promiseValues = [];
+    for (let i = 0; i < args.length; i += 1) {
+      Promise.resolve(args[i]).then(
+        (value) => {
+          promiseValues.push(value);
+        },
+        (err) => reject(err),
+      );
+    }
+    resolve(promiseValues);
+  }
+});
 
 export { allPromises };
