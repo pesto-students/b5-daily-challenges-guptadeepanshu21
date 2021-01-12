84fbe8fc70639c482cd9db117248220f9adbe2a6
Deepanshu Gupta <guptadeepanshu21@gmail.com>
2021-01-12 22:42:59 +0530

    updated variable name

--- js-exercises/all-promises/allPromises.js
+++ js-exercises/all-promises/allPromises.js
@@ -1,15 +1,15 @@
-const allPromises = (args) => new Promise((resolve, reject) => {
-  if (args && args.length) {
-    const promiseValues = [];
-    for (let i = 0; i < args.length; i += 1) {
-      Promise.resolve(args[i]).then(
+const allPromises = (promises) => new Promise((resolve, reject) => {
+  if (promises && promises.length) {
+    const resolvedPromises = [];
+    for (let i = 0; i < promises.length; i += 1) {
+      Promise.resolve(promises[i]).then(
         (value) => {
-          promiseValues.push(value);
+          resolvedPromises.push(value);
         },
         (err) => reject(err),
       );
     }
-    resolve(promiseValues);
+    resolve(resolvedPromises);
   }
 });
 
